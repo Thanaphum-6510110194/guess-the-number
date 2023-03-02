559fdf7daed985115baac96abf293c5fe0e20c3a
ภคพล แดงมณีกุล <6510110334@psu.ac.th>
2023-03-02 20:56:07 +0700

    client - define a function to get user guess

--- GN_Client.py
+++ GN_Client.py
@@ -2,3 +2,15 @@ import socket
 
 HOST = '127.0.0.1' # The server's hostname or IP address
 PORT = 65432    # The port used by the server
+
+def get_guess():
+    while True:
+        try:
+          guess = int(input("Enter your guess: "))
+          if guess < 1 or guess > 100:
+            raise ValueError("Guess must be between 1 and 100")
+          return guess
+        
+        except ValueError as E:
+           print(str(E))
+           
